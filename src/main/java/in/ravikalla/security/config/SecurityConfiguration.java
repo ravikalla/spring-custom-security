0b60b36c6a4562f8ba29796435b07d9e0fe96fbd
Ravi Shankar Kalla <ravi523096@yahoo.com>
2025-06-02 17:32:32 -0700

    Upgrade project to Spring Boot 3

--- src/main/java/in/ravikalla/security/config/SecurityConfiguration.java
+++ src/main/java/in/ravikalla/security/config/SecurityConfiguration.java
@@ -3,18 +3,23 @@ package in.ravikalla.security.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+import in.ravikalla.security.config.CustomBasicAuthenticationEntryPoint;
+import in.ravikalla.security.config.CustomAuthenticationProvider;
+
+@EnableMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
 	private Logger logger = LoggerFactory.getLogger(SecurityConfiguration.class);
 
@@ -26,28 +31,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
     private CustomAuthenticationProvider demoAuthenticationProvider;
-	public void setDemoAuthenticationProvider(CustomAuthenticationProvider demoAuthenticationProvider) {
-		this.demoAuthenticationProvider = demoAuthenticationProvider;
-	}
+        public void setDemoAuthenticationProvider(CustomAuthenticationProvider demoAuthenticationProvider) {
+                this.demoAuthenticationProvider = demoAuthenticationProvider;
+        }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    	auth.authenticationProvider(demoAuthenticationProvider);
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.authenticationProvider(demoAuthenticationProvider);
+        return builder.build();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        logger.info("43 : Start : SecurityConfiguration.filterChain(...)");
 
-    	logger.info("43 : Start : SecurityConfiguration.configure(...)");
-
-        http.csrf().disable();
-        http.authorizeRequests().antMatchers("**/secured/**").permitAll()
-        .anyRequest().authenticated()
-        .and()
-        .httpBasic()
-        .authenticationEntryPoint(customBasicAuthenticationEntryPoint);
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("**/secured/**").permitAll()
+                .anyRequest().authenticated());
+        http.httpBasic(basic -> basic.authenticationEntryPoint(customBasicAuthenticationEntryPoint));
         http.addFilterBefore(new CustomAuthenticationFilter(), BasicAuthenticationFilter.class);
 
-        logger.info("53 : End : SecurityConfiguration.configure(...)");
+        logger.info("53 : End : SecurityConfiguration.filterChain(...)");
+        return http.build();
     }
 }
